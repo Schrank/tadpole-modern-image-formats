230ce89dd9c958f3d15b4493e7f178ff99ac8df4
Björn Meyer <b.meyer@shopware.com>
2023-05-23 11:40:43 +0200

    moved from console command to service, improved template

--- src/Command/ImageConverter.php
+++ src/Command/ImageConverter.php
@@ -3,16 +3,6 @@ declare(strict_types=1);
 
 namespace Tadpole\ModernImageFormats\Command;
 
-use League\Flysystem\FilesystemOperator;
-use Shopware\Core\Content\Media\Aggregate\MediaFolderConfiguration\MediaFolderConfigurationEntity;
-use Shopware\Core\Content\Media\Aggregate\MediaThumbnail\MediaThumbnailEntity;
-use Shopware\Core\Content\Media\Aggregate\MediaThumbnailSize\MediaThumbnailSizeEntity;
-use Shopware\Core\Content\Media\Exception\ThumbnailCouldNotBeSavedException;
-use Shopware\Core\Content\Media\Exception\ThumbnailNotSupportedException;
-use Shopware\Core\Content\Media\MediaEntity;
-use Shopware\Core\Content\Media\MediaType\ImageType;
-use Shopware\Core\Content\Media\MediaType\MediaType;
-use Shopware\Core\Content\Media\Pathname\UrlGeneratorInterface;
 use Shopware\Core\Framework\Adapter\Console\ShopwareStyle;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\RepositoryIterator;
@@ -21,7 +11,9 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Tadpole\ModernImageFormats\Service\ImageConverter as ImageConverterService;
 
 #[AsCommand(
     name: 'media:image:convert',
@@ -29,14 +21,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class ImageConverter extends Command
 {
+
     private int $batchSize = 50;
-    private ShopwareStyle $io;
 
     public function __construct(
         private readonly EntityRepository      $mediaThumbnailRepository,
-        private readonly UrlGeneratorInterface $urlGenerator,
-        private readonly FilesystemOperator    $filesystemPublic,
-        private readonly FilesystemOperator    $filesystemPrivate,
+        private readonly ImageConverterService $imageConverterService,
         string                                 $name = null)
     {
         parent::__construct($name);
@@ -44,34 +34,39 @@ class ImageConverter extends Command
 
     protected function configure(): void
     {
-
+//        $this->addOption(
+//            'strict',
+//            's',
+//            InputOption::VALUE_NONE,
+//            'Additionally checks that physical files for existing thumbnails are present'
+//        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->io = new ShopwareStyle($input, $output);
-        $this->convertToWebp();
+        $io = new ShopwareStyle($input, $output);
+
+        $context = Context::createDefaultContext();
+        $mediaThumbnailIterator = new RepositoryIterator(
+            $this->mediaThumbnailRepository,
+            $context,
+            $this->createThumbnailCriteria()
+        );
+
+        $totalMediaThumbnailCount = $mediaThumbnailIterator->getTotal();
+        $io->comment(
+            sprintf('Generating webp images for %d thumbnails. This may take some time...', $totalMediaThumbnailCount)
+        );
+        $io->progressStart($totalMediaThumbnailCount);
+
+        $this->imageConverterService->generateWebpImages($mediaThumbnailIterator, $io);
+
+        $io->progressFinish();
 
         return 0;
     }
 
-    private function convertToWebp(): void
-    {
-        $context = Context::createDefaultContext();
-        $mediaThumbnailIterator = new RepositoryIterator($this->mediaThumbnailRepository, $context, $this->createCriteria());
-
-        $totalMediaThumbnailCount = $mediaThumbnailIterator->getTotal();
-        $this->io->comment(
-            sprintf('Generating webp images for %d thumbnails. This may take some time...', $totalMediaThumbnailCount)
-        );
-        $this->io->progressStart($totalMediaThumbnailCount);
-
-        $result = $this->generateWebpImages($mediaThumbnailIterator, $context);
-
-        $this->io->progressFinish();
-    }
-
-    private function createCriteria(): Criteria
+    private function createThumbnailCriteria(): Criteria
     {
         $criteria = new Criteria();
         $criteria->setOffset(0);
@@ -81,168 +76,5 @@ class ImageConverter extends Command
         return $criteria;
     }
 
-    private function generateWebpImages(RepositoryIterator $iterator, Context $context): array
-    {
-        while (($result = $iterator->fetch()) !== null) {
-            /** @var MediaThumbnailEntity $mediaThumbnail */
-            foreach ($result->getEntities() as $mediaThumbnail) {
-                $filePath = $this->urlGenerator->getRelativeMediaUrl($mediaThumbnail->getMedia());
-                /** @var string $file */
-                $file = $this->getFileSystem($mediaThumbnail->getMedia())->read($filePath);
-                $image = @imagecreatefromstring($file);
-                if ($image === false) {
-                    throw new ThumbnailNotSupportedException($mediaThumbnail->getId());
-                }
-                $originalImageSize = $this->getOriginalImageSize($image);
-                $expectedthumbnailSize = new MediaThumbnailSizeEntity();
-                $mediaFolder = $mediaThumbnail->getMedia()->getMediaFolder();
-                $expectedthumbnailSize->assign([
-                        'height' => $mediaThumbnail->getHeight(),
-                        'width' => $mediaThumbnail->getWidth(),
-                        'mediaFolderConfigurations' => $mediaFolder->getConfiguration()]
-                );
-                $thumbnailSize = $this->calculateThumbnailSize($originalImageSize, $expectedthumbnailSize, $mediaFolder->getConfiguration());
-                $webpImage = $this->createNewImage($image, $mediaThumbnail->getMedia()->getMediaType(), $originalImageSize, $thumbnailSize);
-                $webpFilePath = $this->urlGenerator->getRelativeThumbnailUrl(
-                        $mediaThumbnail->getMedia(),
-                        $mediaThumbnail
-                    ) . '.webp';
 
-                $this->writeThumbnail($webpImage, $mediaThumbnail->getMedia(), $webpFilePath, 80);
-            }
-            $this->io->progressAdvance($result->count());
-        }
-        return [];
-    }
-
-    /**
-     * @param array{width: int, height: int} $imageSize
-     *
-     * @return array{width: int, height: int}
-     */
-    private function calculateThumbnailSize(
-        array                          $imageSize,
-        MediaThumbnailSizeEntity       $preferredThumbnailSize,
-        MediaFolderConfigurationEntity $config
-    ): array
-    {
-        if (!$config->getKeepAspectRatio() || $preferredThumbnailSize->getWidth() !== $preferredThumbnailSize->getHeight()) {
-            $calculatedWidth = $preferredThumbnailSize->getWidth();
-            $calculatedHeight = $preferredThumbnailSize->getHeight();
-
-            $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
-
-            return $useOriginalSizeInThumbnails ? [
-                'width' => $imageSize['width'],
-                'height' => $imageSize['height'],
-            ] : [
-                'width' => $calculatedWidth,
-                'height' => $calculatedHeight,
-            ];
-        }
-
-        if ($imageSize['width'] >= $imageSize['height']) {
-            $aspectRatio = $imageSize['height'] / $imageSize['width'];
-
-            $calculatedWidth = $preferredThumbnailSize->getWidth();
-            $calculatedHeight = (int)ceil($preferredThumbnailSize->getHeight() * $aspectRatio);
-
-            $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
-
-            return $useOriginalSizeInThumbnails ? [
-                'width' => $imageSize['width'],
-                'height' => $imageSize['height'],
-            ] : [
-                'width' => $calculatedWidth,
-                'height' => $calculatedHeight,
-            ];
-        }
-
-        $aspectRatio = $imageSize['width'] / $imageSize['height'];
-
-        $calculatedWidth = (int)ceil($preferredThumbnailSize->getWidth() * $aspectRatio);
-        $calculatedHeight = $preferredThumbnailSize->getHeight();
-
-        $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
-
-        return $useOriginalSizeInThumbnails ? [
-            'width' => $imageSize['width'],
-            'height' => $imageSize['height'],
-        ] : [
-            'width' => $calculatedWidth,
-            'height' => $calculatedHeight,
-        ];
-    }
-
-    private function getFileSystem(MediaEntity $media): FilesystemOperator
-    {
-        if ($media->isPrivate()) {
-            return $this->filesystemPrivate;
-        }
-
-        return $this->filesystemPublic;
-    }
-
-    /**
-     * @return array{width: int, height: int}
-     */
-    private function getOriginalImageSize(\GdImage $image): array
-    {
-        return [
-            'width' => imagesx($image),
-            'height' => imagesy($image),
-        ];
-    }
-
-    private function createNewImage(\GdImage $mediaImage, MediaType $type, array $originalImageSize, array $thumbnailSize): \GdImage
-    {
-        $thumbnail = imagecreatetruecolor($thumbnailSize['width'], $thumbnailSize['height']);
-
-        if ($thumbnail === false) {
-            throw new \RuntimeException('Can not create image handle');
-        }
-
-        if (!$type->is(ImageType::TRANSPARENT)) {
-            $colorWhite = (int)imagecolorallocate($thumbnail, 255, 255, 255);
-            imagefill($thumbnail, 0, 0, $colorWhite);
-        } else {
-            imagealphablending($thumbnail, false);
-        }
-
-        imagesavealpha($thumbnail, true);
-        imagecopyresampled(
-            $thumbnail,
-            $mediaImage,
-            0,
-            0,
-            0,
-            0,
-            $thumbnailSize['width'],
-            $thumbnailSize['height'],
-            $originalImageSize['width'],
-            $originalImageSize['height']
-        );
-
-        return $thumbnail;
-    }
-
-    private function writeThumbnail(\GdImage $thumbnail, MediaEntity $media, string $filePath, int $quality): void
-    {
-        ob_start();
-
-        if (!\function_exists('imagewebp')) {
-            throw new ThumbnailCouldNotBeSavedException($filePath);
-        }
-
-        imagewebp($thumbnail, null, $quality);
-
-        $imageFile = ob_get_contents();
-        ob_end_clean();
-
-        try {
-            $this->getFileSystem($media)->write($filePath, (string)$imageFile);
-        } catch (\Exception) {
-            throw new ThumbnailCouldNotBeSavedException($filePath);
-        }
-    }
 }
--- src/Service/ImageConverter.php
+++ src/Service/ImageConverter.php
@@ -3,133 +3,196 @@ declare(strict_types=1);
 
 namespace Tadpole\ModernImageFormats\Service;
 
+use League\Flysystem\FilesystemOperator;
+use Shopware\Core\Content\Media\Aggregate\MediaFolderConfiguration\MediaFolderConfigurationEntity;
 use Shopware\Core\Content\Media\Aggregate\MediaThumbnail\MediaThumbnailEntity;
+use Shopware\Core\Content\Media\Aggregate\MediaThumbnailSize\MediaThumbnailSizeEntity;
+use Shopware\Core\Content\Media\Exception\ThumbnailCouldNotBeSavedException;
 use Shopware\Core\Content\Media\Exception\ThumbnailNotSupportedException;
 use Shopware\Core\Content\Media\MediaEntity;
+use Shopware\Core\Content\Media\MediaType\ImageType;
+use Shopware\Core\Content\Media\MediaType\MediaType;
 use Shopware\Core\Content\Media\Pathname\UrlGeneratorInterface;
-use Shopware\Core\Framework\Adapter\Console\ShopwareStyle;
-use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\RepositoryIterator;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ImageConverter
 {
+    private int $batchSize = 50;
+
     public function __construct(
         private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly FilesystemOperator    $filesystemPublic,
+        private readonly FilesystemOperator    $filesystemPrivate,
     )
     {
 
     }
 
-    private function convertToWebp(): void
-    {
-        $context = Context::createDefaultContext();
-        $mediaIterator = new RepositoryIterator($this->mediaRepository, $context, $this->createCriteria());
-
-        $totalMediaCount = $mediaIterator->getTotal();
-        $this->io->comment(sprintf('Generating avif and webp images for %d files. This may take some time...', $totalMediaCount));
-        $this->io->progressStart($totalMediaCount);
-
-        $result = $this->generateWebpImages($mediaIterator, $context);
-
-        $this->io->progressFinish();
-        $this->io->table(
-            ['Action', 'Number of Media Entities'],
-            [
-                ['Generated', $result['generated']],
-                ['Skipped', $result['skipped']],
-                ['Errors', $result['errored']],
-            ]
-        );
-
-        if (is_countable($result['errors']) ? \count($result['errors']) : 0) {
-            if ($this->io->isVerbose()) {
-                $this->io->table(
-                    ['Error messages'],
-                    $result['errors']
-                );
-            } else {
-                $this->io->warning(\sprintf('Thumbnail generation for %d file(s) failed. Use -v to show the files', is_countable($result['errors']) ? \count($result['errors']) : 0));
-            }
-        }
-    }
-
-    private function createCriteria(): Criteria
-    {
-        $criteria = new Criteria();
-        $criteria->setOffset(0);
-        $criteria->setLimit($this->batchSize);
-        $criteria->addFilter(new EqualsFilter('media.mediaFolder.configuration.createThumbnails', true));
-        $criteria->addAssociation('thumbnails');
-        $criteria->addAssociation('mediaFolder.configuration.mediaThumbnailSizes');
-
-        return $criteria;
-    }
-
-    private function generateWebpImages(RepositoryIterator $iterator, Context $context): array
+    public function generateWebpImages(RepositoryIterator $iterator, $io): array
     {
         while (($result = $iterator->fetch()) !== null) {
-            /** @var MediaEntity $media */
-            foreach ($result->getEntities() as $media) {
-                $filePath = $this->urlGenerator->getRelativeMediaUrl($media);
+            /** @var MediaThumbnailEntity $mediaThumbnail */
+            foreach ($result->getEntities() as $mediaThumbnail) {
+                $filePath = $this->urlGenerator->getRelativeMediaUrl($mediaThumbnail->getMedia());
                 /** @var string $file */
-                $file = $this->getFileSystem($media)->read($filePath);
+                $file = $this->getFileSystem($mediaThumbnail->getMedia())->read($filePath);
                 $image = @imagecreatefromstring($file);
                 if ($image === false) {
-                    throw new ThumbnailNotSupportedException($media->getId());
+                    throw new ThumbnailNotSupportedException($mediaThumbnail->getId());
                 }
-                $url = $this->urlGenerator->getRelativeThumbnailUrl(
-                    $media,
-                    (new MediaThumbnailEntity())->assign(['width' => , 'height' => $size->getHeight()])
+                $originalImageSize = $this->getOriginalImageSize($image);
+                $expectedThumbnailSize = new MediaThumbnailSizeEntity();
+                $mediaFolder = $mediaThumbnail->getMedia()->getMediaFolder();
+                $expectedThumbnailSize->assign([
+                        'height' => $mediaThumbnail->getHeight(),
+                        'width' => $mediaThumbnail->getWidth(),
+                        'mediaFolderConfigurations' => $mediaFolder->getConfiguration()]
                 );
-                imagewebp($image, $thumbnailPath);
+                $thumbnailSize = $this->calculateThumbnailSize($originalImageSize, $expectedThumbnailSize, $mediaFolder->getConfiguration());
+                $webpImage = $this->createNewImage($image, $mediaThumbnail->getMedia()->getMediaType(), $originalImageSize, $thumbnailSize);
+                $webpFilePath = $this->urlGenerator->getRelativeThumbnailUrl(
+                        $mediaThumbnail->getMedia(),
+                        $mediaThumbnail
+                    ) . '.webp';
 
+                $this->writeThumbnail($webpImage, $mediaThumbnail->getMedia(), $webpFilePath, 80);
             }
-            $this->io->progressAdvance($result->count());
-
-
-            // TODO fix return to be array
-
+            $io->progressAdvance($result->count());
         }
         return [];
+    }
 
+    /**
+     * @param array{width: int, height: int} $imageSize
+     *
+     * @return array{width: int, height: int}
+     */
+    private function calculateThumbnailSize(
+        array                          $imageSize,
+        MediaThumbnailSizeEntity       $preferredThumbnailSize,
+        MediaFolderConfigurationEntity $config
+    ): array
+    {
+        if (
+            !$config->getKeepAspectRatio() ||
+            $preferredThumbnailSize->getWidth() !== $preferredThumbnailSize->getHeight()
+        ) {
+            $calculatedWidth = $preferredThumbnailSize->getWidth();
+            $calculatedHeight = $preferredThumbnailSize->getHeight();
 
-        return 1;
+            $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
 
-        $generated = 0;
-        $skipped = 0;
-        $errored = 0;
-        $errors = [];
-
-        while (($result = $iterator->fetch()) !== null) {
-            /** @var MediaEntity $media */
-            foreach ($result->getEntities() as $media) {
-                try {
-                    if ($this->thumbnailService->updateThumbnails($media, $context, $this->isStrict) > 0) {
-                        ++$generated;
-                    } else {
-                        ++$skipped;
-                    }
-                } catch (\Throwable $e) {
-                    ++$errored;
-                    $errors[] = [sprintf('Cannot process file %s (id: %s) due error: %s', $media->getFileName(), $media->getId(), $e->getMessage())];
-                }
-            }
-            $this->io->progressAdvance($result->count());
+            return $useOriginalSizeInThumbnails ? [
+                'width' => $imageSize['width'],
+                'height' => $imageSize['height'],
+            ] : [
+                'width' => $calculatedWidth,
+                'height' => $calculatedHeight,
+            ];
         }
 
-        return [
-            'generated' => $generated,
-            'skipped' => $skipped,
-            'errored' => $errored,
-            'errors' => $errors,
-        ];
+        if ($imageSize['width'] >= $imageSize['height']) {
+            $aspectRatio = $imageSize['height'] / $imageSize['width'];
 
+            $calculatedWidth = $preferredThumbnailSize->getWidth();
+            $calculatedHeight = (int)ceil($preferredThumbnailSize->getHeight() * $aspectRatio);
+
+            $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
+
+            return $useOriginalSizeInThumbnails ? [
+                'width' => $imageSize['width'],
+                'height' => $imageSize['height'],
+            ] : [
+                'width' => $calculatedWidth,
+                'height' => $calculatedHeight,
+            ];
+        }
+
+        $aspectRatio = $imageSize['width'] / $imageSize['height'];
+
+        $calculatedWidth = (int)ceil($preferredThumbnailSize->getWidth() * $aspectRatio);
+        $calculatedHeight = $preferredThumbnailSize->getHeight();
+
+        $useOriginalSizeInThumbnails = $imageSize['width'] < $calculatedWidth || $imageSize['height'] < $calculatedHeight;
+
+        return $useOriginalSizeInThumbnails ? [
+            'width' => $imageSize['width'],
+            'height' => $imageSize['height'],
+        ] : [
+            'width' => $calculatedWidth,
+            'height' => $calculatedHeight,
+        ];
+    }
+
+    private function getFileSystem(MediaEntity $media): FilesystemOperator
+    {
+        if ($media->isPrivate()) {
+            return $this->filesystemPrivate;
+        }
+
+        return $this->filesystemPublic;
+    }
+
+    /**
+     * @return array{width: int, height: int}
+     */
+    private function getOriginalImageSize(\GdImage $image): array
+    {
+        return [
+            'width' => imagesx($image),
+            'height' => imagesy($image),
+        ];
+    }
+
+    private function createNewImage(\GdImage $mediaImage, MediaType $type, array $originalImageSize, array $thumbnailSize): \GdImage
+    {
+        $thumbnail = imagecreatetruecolor($thumbnailSize['width'], $thumbnailSize['height']);
+
+        if ($thumbnail === false) {
+            throw new \RuntimeException('Can not create image handle');
+        }
+
+        if (!$type->is(ImageType::TRANSPARENT)) {
+            $colorWhite = (int)imagecolorallocate($thumbnail, 255, 255, 255);
+            imagefill($thumbnail, 0, 0, $colorWhite);
+        } else {
+            imagealphablending($thumbnail, false);
+        }
+
+        imagesavealpha($thumbnail, true);
+        imagecopyresampled(
+            $thumbnail,
+            $mediaImage,
+            0,
+            0,
+            0,
+            0,
+            $thumbnailSize['width'],
+            $thumbnailSize['height'],
+            $originalImageSize['width'],
+            $originalImageSize['height']
+        );
+
+        return $thumbnail;
+    }
+
+    private function writeThumbnail(\GdImage $thumbnail, MediaEntity $media, string $filePath, int $quality): void
+    {
+        ob_start();
+
+        if (!\function_exists('imagewebp')) {
+            throw new ThumbnailCouldNotBeSavedException($filePath);
+        }
+
+        imagewebp($thumbnail, null, $quality);
+
+        $imageFile = ob_get_contents();
+        ob_end_clean();
+
+        try {
+            $this->getFileSystem($media)->write($filePath, (string)$imageFile);
+        } catch (\Exception) {
+            throw new ThumbnailCouldNotBeSavedException($filePath);
+        }
     }
 }
